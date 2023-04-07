5c6ad289db4f15b0b9888000ffe2c3b2b6019ebc
sembomar <marek.sembol@tietoevry.com>
2023-04-07 16:56:03 +0200

    Fix?

--- internal/provider/oidc.go
+++ internal/provider/oidc.go
@@ -45,14 +45,14 @@ func (o *OIDC) Setup() error {
 	myLog := log.New(io.Discard, config.Prefix + ": ", log.Lmsgprefix|log.Ldate|log.Ltime)
 	myLog.SetOutput(os.Stdout)
 	myLog.Println("Haaaaaaaaaaaaaaaaaaleluja - aspon neco")
+	o.MyLog = myLog
 
 	// Create oauth2 config
 	o.Config = &oauth2.Config{
 		ClientID:     o.ClientID,
 		ClientSecret: o.ClientSecret,
 		Endpoint:     o.provider.Endpoint(),
-		MyLog: myLog,
-
+	
 		// "openid" is a required scope for OpenID Connect flows.
 		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
 	}
