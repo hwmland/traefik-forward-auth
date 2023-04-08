5ceb3507e4f4b5afb8bb94635e3e4720a43c3154
sembomar <marek.sembol@tietoevry.com>
2023-04-08 17:04:56 +0200

    fix build

--- internal/provider/oidc_test.go
+++ internal/provider/oidc_test.go
@@ -78,7 +78,7 @@ func TestOIDCGetLoginURL(t *testing.T) {
 		"client_id":     []string{"idtest"},
 		"redirect_uri":  []string{"http://example.com/_oauth"},
 		"response_type": []string{"code"},
-		"scope":         []string{"openid profile email"},
+		"scope":         []string{"openid profile email groups"},
 		"state":         []string{"state"},
 		"resource":      []string{"resourcetest"},
 	}
