2cdc141b0d3051607937bc384039101bcaf61df9
sembomar <marek.sembol@tietoevry.com>
2023-04-09 12:10:19 +0200

    fix build

--- internal/provider/oidc.go
+++ internal/provider/oidc.go
@@ -117,5 +117,5 @@ func (o *OIDC) GetUser(token, _ string) (*User, error) {
 	// }
 	// uniqueGrops := mapKeys(groupMap)
 
-	return &User{User: user.Email, Roles: userroles, }, nil
+	return &User{User: user.Email, Roles: usre.Roles, }, nil
 }
