9fb50768c456aeca3891501d457f9808e86ed4e7
sembomar <marek.sembol@tietoevry.com>
2023-04-09 10:55:36 +0200

    fix build

--- internal/server.go
+++ internal/server.go
@@ -227,7 +227,7 @@ func (s *Server) AuthCallbackHandler() http.HandlerFunc {
 			return
 		}
 
-		logger.WithField("token": token).Debug("------>")
+		logger.WithField("token", token).Debug("------>")
 		// Get user
 		user, err := p.GetUser(token, config.UserPath)
 		if err != nil {
